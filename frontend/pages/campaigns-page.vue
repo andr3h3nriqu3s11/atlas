20616b9f787285ac7ce41c07a5ee12ed59ee85e4
OliverKrieger <oliver.krieger76@gmail.com>
2023-01-01 21:03:20 +0000

    add tab stylings, functionality WIP

--- frontend/pages/campaigns-page.vue
+++ frontend/pages/campaigns-page.vue
@@ -3,3 +3,13 @@
         <h2>Campaign Creator</h2>
     </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data() {
+        return {
+        }
+    }
+})
+</script>
--- frontend/pages/characters-page.vue
+++ frontend/pages/characters-page.vue
@@ -1,15 +1,79 @@
 <template>
     <div class="characters-page">
-        <div class="generic-form">
-            <select name="settings" id="settings">
-                <option value="all">All</option>
-                <option value="fifthedition">5th Edition</option>
-                <option value="savageworlds">Savage Worlds</option>
-            </select>
+        <div class="character-tabs">
+            <div class="btn" v-for="(tab, index) in tabs" :key="index">
+                {{tab.name}}
+            </div>
+            <div class="square-btn btn">
+                <span>+</span>
+            </div>
         </div>
-        <div class="character-flex">
-            <CharacterAddNewCharacter />
-            <CharacterListCharacters />
+        <div class="character-select" v-if="this.active">
+            <div class="generic-form">
+                <select name="settings" id="settings">
+                    <option value="all">All</option>
+                    <option value="fifthedition">5th Edition</option>
+                    <option value="savageworlds">Savage Worlds</option>
+                </select>
+            </div>
+            <div class="character-flex">
+                <CharacterAddNewCharacter />
+                <CharacterListCharacters />
+            </div>
         </div>
     </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data() {
+        return {
+            active:false,
+            tabs:[
+                {
+                    "name":"Veraxhadon"
+                },
+                {
+                    "name":"The Rogue"
+                },
+                {
+                    "name":"The Red One"
+                },
+                {
+                    "name":"The Eternal Emperor"
+                },
+                {
+                    "name":"Veraxhadon"
+                },
+                {
+                    "name":"The Rogue"
+                },
+                {
+                    "name":"The Red One"
+                },
+                {
+                    "name":"The Eternal Emperor"
+                },
+                {
+                    "name":"Veraxhadon"
+                },
+                {
+                    "name":"The Rogue"
+                },
+                {
+                    "name":"The Red One"
+                },
+                {
+                    "name":"The Eternal Emperor"
+                }
+            ]
+        }
+    },
+    methods:{
+        switchActive(){
+            this.active = !this.active
+        }
+    }
+})
+</script>
