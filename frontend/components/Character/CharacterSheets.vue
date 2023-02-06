aec835071336dd88edd7069f8c0c27d1d412daa4
OliverKrieger <oliver.krieger76@gmail.com>
2023-02-06 22:18:49 +0000

    Character sheets WIP

---
+++ frontend/components/Character/CharacterSheets.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="character-sheets-component">
+
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+    },
+    props:["character_ids"]
+})
+</script>
